a43ba61d31925262190252ababc6e264d17c2611
Aldo Gabriel <gabrielaldo42@gmail.com>
2025-04-03 13:25:23 +0700

    feat(user): search product

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,8 @@ Route::get('/shop/{product_slug}', [ShopController::class, 'product_details'])->
 Route::get('/contact-us', [HomeController::class, 'contact'])->name('home.contact');
 Route::post('/contact/store', [HomeController::class, 'contact_store'])->name('home.contact.store');
 
+Route::get('/search', [HomeController::class, 'search'])->name('home.search');
+
 Route::middleware(['auth'])->group(function () {
     Route::get('/account-dashboard', [UserController::class, 'index'])->name('user.index');
 });
